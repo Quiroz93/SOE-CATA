e9779c4fc1041e3aa5f298b16550dfa384651ed5
José <jose.quirozquiroz93@gmail.com>
2026-02-14 12:38:11 -0500

    Instala y configura Spatie Laravel Permission

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
+
+import { createApp } from 'vue';
 import './bootstrap';
+
+import ExampleComponent from './components/ExampleComponent.vue';
+
+const app = createApp({});
+app.component('example-component', ExampleComponent);
+app.mount('#app');
