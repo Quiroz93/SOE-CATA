77dd8584ed9f2748b76a9f4255bdebd6c22b2c95
José <jose.quirozquiroz93@gmail.com>
2026-02-16 17:15:53 -0500

    refactor: implementación FormRequest módulo Competencia

--- app/Http/Controllers/Admin/CompetenciaController.php
+++ app/Http/Controllers/Admin/CompetenciaController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Competencia;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreCompetenciaRequest;
+use App\Http\Requests\UpdateCompetenciaRequest;
 
 class CompetenciaController extends Controller
 {
     public function index() { return view('admin.competencias.index'); }
     public function show(Competencia $competencia) { return view('admin.competencias.show'); }
     public function create() { return view('admin.competencias.create'); }
-    public function store(Request $request) { return view('admin.competencias.index'); }
+    public function store(StoreCompetenciaRequest $request) { return view('admin.competencias.index'); }
     public function edit(Competencia $competencia) { return view('admin.competencias.edit'); }
-    public function update(Request $request, Competencia $competencia) { return view('admin.competencias.index'); }
+    public function update(UpdateCompetenciaRequest $request, Competencia $competencia) { return view('admin.competencias.index'); }
     public function destroy(Competencia $competencia) { return view('admin.competencias.index'); }
 }
