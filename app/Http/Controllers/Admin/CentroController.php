a6cfe066094dcb5f0495d92c39b96eb1f83ffe95
José <jose.quirozquiroz93@gmail.com>
2026-02-16 15:51:42 -0500

    refactor: implementación FormRequest módulo Centro

--- app/Http/Controllers/Admin/CentroController.php
+++ app/Http/Controllers/Admin/CentroController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Centro;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreCentroRequest;
+use App\Http\Requests\UpdateCentroRequest;
 
 class CentroController extends Controller
 {
     public function index() { return view('admin.centros.index'); }
     public function show(Centro $centro) { return view('admin.centros.show'); }
     public function create() { return view('admin.centros.create'); }
-    public function store(Request $request) { return view('admin.centros.index'); }
+    public function store(StoreCentroRequest $request) { return view('admin.centros.index'); }
     public function edit(Centro $centro) { return view('admin.centros.edit'); }
-    public function update(Request $request, Centro $centro) { return view('admin.centros.index'); }
+    public function update(UpdateCentroRequest $request, Centro $centro) { return view('admin.centros.index'); }
     public function destroy(Centro $centro) { return view('admin.centros.index'); }
 }
