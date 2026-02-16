3c3a8f3a03588529c6ed288ecde5d1dc13a3cc6c
José <jose.quirozquiroz93@gmail.com>
2026-02-16 18:03:18 -0500

    refactor: eager loading competencias en módulo público Programa

--- app/Http/Controllers/Public/ProgramaController.php
+++ app/Http/Controllers/Public/ProgramaController.php
@@ -14,6 +14,7 @@ class ProgramaController extends Controller
     }
     public function show(Programa $programa)
     {
+        $programa->load('competencias');
         return view('public.programas.show', compact('programa'));
     }
 }
