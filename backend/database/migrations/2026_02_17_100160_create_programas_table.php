f25c76df674e532adb25d161d7396020bc7ee290
José <jose.quirozquiroz93@gmail.com>
2026-02-19 12:09:30 -0500

    resolucion de conflictos en el .lock de composer y migraciones con campos repetidos

--- backend/database/migrations/2026_02_17_100160_create_programas_table.php
+++ backend/database/migrations/2026_02_17_100160_create_programas_table.php
@@ -14,11 +14,7 @@ return new class extends Migration {
             $table->string('codigo')->unique();
             $table->string('nivel');
             $table->text('descripcion')->nullable();
-<<<<<<<< HEAD:backend/database/migrations/2026_02_14_180200_create_programas_table.php
             $table->string('estado')->default('borrador');
-========
-            $table->string('estado')->default('activo');
->>>>>>>> 890bddfd09077b32064d4f51ce1f25dbb94cbd40:backend/database/migrations/2026_02_17_100160_create_programas_table.php
             $table->timestamps();
             $table->softDeletes();
         });
