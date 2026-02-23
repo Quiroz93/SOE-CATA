7e63f0e7eb0d8e8f15d3aa99f2e460b7589f271a
José <jose.quirozquiroz93@gmail.com>
2026-02-23 11:39:11 -0500

    preparar espacio para logos oficiales del sena en src/components/welcome/WelcomeFeatured.vue

--- frontend/src/components/welcome/WelcomeFeatured.vue
+++ frontend/src/components/welcome/WelcomeFeatured.vue
@@ -3,10 +3,10 @@
     <div class="container">
       <h2>Encuentranos en</h2>
       <div class="logos">
-        <img src="/images/airbnb-logo.svg" alt="Logo 1">
-        <img src="/images/spotify-logo.svg" alt="Logo 2">
-        <img src="/images/google-logo.svg" alt="Logo 3">
-        <img src="/images/canva-logo.svg" alt="Logo 4">
+        <img src="" alt="Logo 1">
+        <img src="" alt="Logo 2">
+        <img src="" alt="Logo 3">
+        <img src="" alt="Logo 4">
       </div>
     </div>
   </section>
